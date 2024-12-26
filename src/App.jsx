353bd606887f755d76cc8e6087d039e242807923
morpheuslord <70637311+morpheuslord@users.noreply.github.com>
2024-12-26 15:25:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,8 @@ import Services from './components/services/Services'
 import Testimonials from './components/testimonials/Testimonials'
 import Research from './components/research/Research'
 import Certs from './components/certifications/Certs'
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const App = () => {
-    <SpeedInsights/>
     return (
         <>
             <Header />
